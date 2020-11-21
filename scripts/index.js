45c60ccf149e63cb62fc4f5a74149541fdcde5d5
MrGrub84 <grub@gypsypebbles.com>
2020-11-20 23:56:02 -0600

    feat: created onLoad for places list

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,55 @@ let name = document.querySelector(".profile__name");
 let about = document.querySelector(".profile__title");
 let popup = document.querySelector(".popup");
 
+const placeTemp = document.querySelector("#placeTemplate").content;
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "./images/places-yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "./images/places-louis.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "./images/places-bald.png"
+    },
+    {
+        name: "Latemar",
+        link: "./images/places-latemar.png"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "./images/places-vanoise.png"
+    },
+    {
+        name: "Lago di Braies",
+        link: "./images/places-lago.png"
+    }
+];
+
+
+function addPlace(name, link) {
+    const places = document.querySelector('.places__list');
+
+    if (name && link) {
+        let template = placeTemp.cloneNode(true);
+        template.querySelector(".places__photo").src = link;
+        template.querySelector(".places__photo").alt = name;
+        template.querySelector(".places__text").textContent = name;
+        places.appendChild(template);
+    } else {
+        return false;
+    }
+}
+
+function onLoad() {
+    initialCards.forEach(function(item) {
+        addPlace(item.name, item.link);
+    });
+}
+
 function closePopup() {
     popup.classList.remove("popup_opened");
 }
@@ -27,3 +76,4 @@ function openPopup() {
 edit.addEventListener("click", openPopup);
 exit.addEventListener("click", closePopup);
 form.addEventListener('submit', saveProfile); 
+onLoad();
