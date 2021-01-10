2a95a715b9a3f8eda593c38e71aa9e1ff235719d
MrGrub84 <grub@gypsypebbles.com>
2021-01-10 08:03:50 -0800

    fixed reviewers comments

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -12,8 +12,6 @@ export default class PopupWithImage extends Popup {
         this._photo.src = "";
         this._photo.src = url;
         this._photo.alt = text;
-        this._elementPopup.classList.add("popup_opened");
-        document.addEventListener("click",  this._handleOverlayClick);
-        document.addEventListener("keydown",  this._handleEscClose);
+        super.open(); 
     }
 }
